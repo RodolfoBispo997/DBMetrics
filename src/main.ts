e15afdb01e7ee0f321b6bc8af4bef57ef16a2ad9
RodolfoBispo997 <RodolfoBispo997@gmail.com>
2026-07-22 12:28:40 -0300

    feat(docs): configurar Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app/app.module";
 import { ValidationPipe } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { DomainExceptionFilter } from "./shared/filters/domain-exception.filter";
 
 async function bootstrap() {
@@ -21,6 +22,16 @@ async function bootstrap() {
   );
 
   app.useGlobalFilters(new DomainExceptionFilter());
+
+  const config = new DocumentBuilder()
+    .setTitle("DBMetrics API")
+    .setDescription("API for monitoring database metrics")
+    .setVersion("1.0")
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("docs", app, document);
 
   await app.listen(3333);
 
